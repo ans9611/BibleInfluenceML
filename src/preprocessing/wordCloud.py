c15e36ec9358245d4a8703412cdc03e013202e57
ans9611 <62779373+ans9611@users.noreply.github.com>
2023-07-19 00:44:12 -0400

    word cloud

--- src/preprocessing/wordCloud.py
+++ src/preprocessing/wordCloud.py
@@ -1,13 +1,29 @@
+import re
 import nltk
 nltk.download('stopwords')
-import glob
 from nltk.corpus import stopwords
+import glob
+import string
+import gensim
+from gensim.corpora import Dictionary
+from gensim.utils import simple_preprocess
+from gensim.models import CoherenceModel, Phrases, LdaModel
+from gensim.models.ldamulticore import LdaMulticore
+import pandas as pd
+from num2words import num2words
+import numpy as np
+from wordcloud import WordCloud
+from sklearn.feature_extraction.text import CountVectorizer
+import seaborn as sbn 
+import matplotlib.pyplot as plt
+from scipy.stats import hmean
+from scipy.stats import norm
 
 # ... (rest of your code)
 
 
 data = [] 
-ifiles = glob.glob("books/*.txt")
+ifiles = glob.glob("../../data/*.txt")
 for ifile in ifiles: 
     book = open(ifile, "r").read().strip() 
     data.append(book)
@@ -36,3 +52,12 @@ for i, book in enumerate(data, 0):
     data[i] = re.sub(r"\\n", " ", data[i])
     # remove stopwords 
     data[i] = ' '.join([word for word in data[i].split() if word not in stop_words]) #remove_stopwords(data[i], stop_words)
+big_string = ""
+for book in data:
+    big_string += book + " "
+    
+wordcloud = WordCloud(width=1600, height=800,max_font_size=200).generate(big_string)
+plt.figure(figsize=(12,10))
+plt.imshow(wordcloud, interpolation="bilinear")
+plt.axis("off")
+plt.show()
